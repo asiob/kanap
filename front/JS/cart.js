de2838f1abb0a35cb8fcabb737b508e7df8957cd
asiob <agnes.siobud@gmail.com>
2022-06-24 12:58:04 -0400

    change24j

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -3,32 +3,105 @@
 
 
 import { afficherPanier} from "./fonction.js";
-import { supPanier} from "./fonction.js";
+import { afficherPrixTotal, validateEmail, verifierPasDeChiffres} from "./fonction.js";
+// import { supPanier} from "./fonction.js";
 
 
 
 // calcul du total
-// let cartPrice = document.querySelector(".cart__price");
+let cartPrice = document.querySelector(".cart__price");
 
 let spanQty = document.querySelector('#totalQuantity');
 let spanPrice = document.querySelector('#totalPrice');
-afficherPanier(spanQty, spanPrice);
+let element = 0;
+let qteTotal = 0;
+let prixTotal = 0;
+afficherPanier();
 
-//aller chercher les prix dans le panier
-//  for ( let p = 0; p < product; p++) {
-    //   let panier = product[p].price;
-    
-    //   console.log(product[p].price);
-    //  }
-    // console.log(spanQty);
-    
+  
        
 
+// Récupérer tous les boutons qui ont la classe deleteItem
+let deleteItems = document.querySelectorAll(".deleteItem") ;
 
-let deleteItems = document.querySelectorAll (".deleteItem") ;
+//recuperer le panier dans le localstorage
+let panier = JSON.parse(localStorage.getItem("panier"));
+console.log(panier);
+
+console.log(deleteItems);
+// Parcourir le tableau des boutons
 for(let i = 0; i < deleteItems.length; i++){
-    deleteItems[i].addEventListener (`click`, supPanier(deleteItems[i]))
+    deleteItems[i].addEventListener ('click', function(){
+        let articlePanier = deleteItems[i].closest("article");
+    
+        // la supprimer
+        articlePanier.remove() ;
+
+        // creer une variable du produit sup
+        let indiceProduitSupp = i;
+
+        // Mettre  jour le panier
+        console.log(panier[indiceProduitSupp]);
+        //parcourir le panier pour voir le produit supprimé
+        let eltSupp = panier.splice(indiceProduitSupp, 1);
+        console.log(eltSupp);
+        //supprimer du localstorage le produit sup de la page html
+        localStorage.setItem('panier', JSON.stringify(panier));
+    })
 }
 
+// aller chercher les prix dans le panier
+for ( let p = 0; p < panier.length; p++) {
+    qteTotal += panier[p].qty; // qteTotal = qteTotal + panier[p].qty
+    prixTotal += panier[p].price; 
+}
+
+afficherPrixTotal(cartPrice, qteTotal, prixTotal);
+
+//gerer la modification du lot dans le panier
+let choixQty = document.querySelectorAll (".itemQuantity")
+console.log(choixQty);
+for (let i = 0; i < choixQty.length; i++) {
+    choixQty[i].addEventListener ('input', function(){
+        // panier = .findIndex ();
+        let indice = i ; 
+        let nouvelleQty = Number(choixQty[i]["value"]);
+        panier[indice].qty = nouvelleQty;
+        localStorage.setItem('panier', JSON.stringify(panier));
+    });
+    
+};
+
+// traitement du formulaire
+let btnCommande = document.querySelector("#order");
+let inputPrenom = document.querySelector("#firstName");
+let inputNom  = document.querySelector("#lastName");
+let inputAdresse = document.querySelector("#address");
+let inputVille = document.querySelector("#city");
+let inputMail = document.querySelector("#email");
+
+console.log(btnCommande);
+
+btnCommande.addEventListener('click', function(event){
+    // annule le comportement par defaut du formulaire qui est d'envoyer des donnees au serveur
+    event.preventDefault ();
 
 
+    // recuperer les valeurs du formulaire
+    let prenom = inputPrenom.value;
+    let nom  = inputNom.value;
+    console.log(nom);
+    let adresse =inputAdresse.value;
+    let ville = inputVille.value;
+    let mail = inputMail.value;
+    // console.log(inputNom.value, inputPrenom.value,inputAdresse.value, inputVille.value, inputMail.value );
+    let isValid = verifierPasDeChiffres(nom);
+    console.log(isValid);
+
+    let isValidEmail = validateEmail(mail);
+    console.log(isValidEmail);
+});
+
+
+
+//  localStorage.setItem( "firstName", document.querySelector("#firstName").value);
--- front/JS/fonction.js
+++ front/JS/fonction.js
@@ -36,99 +36,55 @@ function ajouterPanier(product, panier) {
   
 }
 
-//ancienne version du ajouter panier
-// function ajouterPanier(product) {
-//   console.log(product);
 
-//   let produitsDansLePanier = JSON.parse (localStorage.getItem('panier'));
-  
-  
-//   if (produitsDansLePanier !== null ) {
-//     products.push(product);
-//     localStorage.setItem("panier", JSON.stringify(products));
-//   }
-
-  
-//   if (produitsDansLePanier !== null ) {
-//     products = produitsDansLePanier;
-//     //Vérifier si le produit qu'on ajoute n'est pas déjà dans le panier
-//     produitsDansLePanier.forEach(p => {
-//       if(product._id == p._id && product.color == p.color){
-        
-//         p.qty = Number(p.qty) + Number(product.qty);
-//         localStorage.setItem('panier', JSON.stringify(produitsDansLePanier));
-//       }
-//     });
-//   } else {
-    
-//     products.push(product);
-//     localStorage.setItem("panier", JSON.stringify(products)); 
-//   }
-// }
-
-
-
-
-
-
-  const afficherPanier = (spanQty, spanPrice) => {
-    //recuperer le panier du localstorage
+  const afficherPanier = () => {
+    // //recuperer le panier du localstorage
     let prixTotal = 0;
     let qteTotal = 0;
     let panier = JSON.parse(localStorage.getItem('panier'));
     console.log(panier); 
-    let section = document.querySelector('#cart__items');
+    let section = document.getElementById('cart__items');
     console.log(section);
     panier.forEach (produit => 
     {
-      afficherProduitDuPanier (produit, section);
-      prixTotal += produit.price;
-      qteTotal += produit.qty;
-      spanQty.innerHTML = prixTotal;
-      spanPrice.innerHTML = qteTotal;
+      // afficherProduitDuPanier (produit, section);
+      section.innerHTML += `
+      <article class="cart__item" data-id="${produit._id}" data-color="${produit.color}">
+        <div class="cart__item__img">
+          <img src="${produit.imageUrl}" alt="Photographie d'un canapé">
+        </div>
+        <div class="cart__item__content">
+          <div class="cart__item__content__description">
+            <h2>${produit.altTxt}</h2>
+            <p>${produit.color}</p>
+            <p>${produit.price} €</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <p>Qté : </p>
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${produit.qty}">
+            </div>
+            <div class="cart__item__content__settings__delete">
+              <p class="deleteItem">Supprimer</p>
+            </div>
+          </div>
+        </div>
+      </article>
+      `
     });
   }
+  
 
+function afficherPrixTotal (element, qteTotal, prixTotal) {
 
-  function afficherProduitDuPanier(produit, element) {
-   
-      element.innerHTML += `
-    
-              <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-                <div class="cart__item__img">
-                  <img src="${produit.imageUrl}" alt="Photographie d'un canapé">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${produit.altTxt}</h2>
-                    <p>${produit.color}</p>
-                    <p>${produit.price} €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté :${produit.qty} </p>
-                      <!--<input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">-->
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article>
-            
-            `
+element.innerHTML =
+`
+<p>
+  Total (<span id="totalQuantity">${qteTotal}</span> articles) : <span id="totalPrice">${prixTotal}</span> €
+</p>
+`
+
 }
-
-// function afficherPrixTotal (element, qteTotal, prixTotal) {
-
-// element.innerHTML =
-// `
-// <p>
-//   Total (<span id="totalQuantity">${qteTotal}</span> articles) : <span id="totalPrice">${prixTotal}</span> €
-// </p>
-// `
-
-// }
 
 //fonction du bouton supprimer pour enlever du panier un produit
 
@@ -140,7 +96,34 @@ function supPanier(btnSup) {
     // la supprimer
     articlePanier.remove() ;
 
+}
 
+
+//-------- verifier les erreurs de caractere dans le formulaire----------
+
+
+function verifierPasDeChiffres (expression) {
+  let sansChiffres = new RegExp('^[a-zà-ï- ]+$');
+  let valid =  sansChiffres.test (expression);
+  console.log(valid);
+
+  if(valid) {
+      return true;
+  } else {
+      return false;
+  }
+
+}
+
+function validateEmail(email){
+  var emailReg = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?', '')
+  var valid = emailReg.test(email);
+
+  if(!valid) {
+      return false;
+  } else {
+      return true;
+  }
 }
 
 
@@ -148,6 +131,8 @@ function supPanier(btnSup) {
     ajouterPanier, 
     afficherPanier, 
     supPanier,
-    // afficherPrixTotal,
-    afficherProduitDuPanier
+    afficherPrixTotal,
+    verifierPasDeChiffres,
+    validateEmail
+ 
   }
